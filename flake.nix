cb909e3d7d2e6a4facf2bfbcd02b18be176d475b
racheedevel <git@rachee.dev>
2025-12-25 15:52:41 +0100

    formatting + zen-browser flakes

--- flake.nix
+++ flake.nix
@@ -13,24 +13,39 @@
       url = "github:nix-community/neovim-nightly-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
 
-  outputs = { self, nixpkgs, home-manager, neovim-nightly-overlay, ... }@inputs: {
-    nixosConfigurations.rachee-fw = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./hosts/framework/configuration.nix
-
-        { nixpkgs.overlays = [ neovim-nightly-overlay.overlays.default ]; }
-
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.rachee = import ./modules/home;
-        }
-      ];
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      neovim-nightly-overlay,
+      zen-browser,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations.rachee-fw = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/framework/configuration.nix
+          ./hosts/framework/system-packages.nix
+
+          { nixpkgs.overlays = [ neovim-nightly-overlay.overlays.default ]; }
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.rachee = import ./modules/home;
+          }
+        ];
+      };
+    };
 }

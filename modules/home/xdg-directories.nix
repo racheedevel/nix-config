fc28428e0c418d749b2f763990e04a14e7079407
racheedevel <git@rachee.dev>
2025-12-27 14:00:33 +0100

    fix directory entry

--- modules/home/xdg-directories.nix
+++ modules/home/xdg-directories.nix
@@ -4,12 +4,13 @@
   xdg.userDirs = {
     enable = true;
     documents = "${config.home.homeDirectory}/docs";
-    downloads = "${config.home.homeDirectory}/xdg/dl";
+    download = "${config.home.homeDirectory}/xdg/dl";
     desktop = "${config.home.homeDirectory}/xdg/desktop";
     music = "${config.home.homeDirectory}/xdg/music";
     pictures = "${config.home.homeDirectory}/xdg/pictures";
     publicShare = "${config.home.homeDirectory}/xdg/share";
     templates = "${config.home.homeDirectory}/xdg/templates";
     videos = "${config.home.homeDirectory}/xdg/videos";
+    # createDirectories = true;
   };
 }

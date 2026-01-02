8eb9d687a6852ab4ee65ad7108b61a1545c01b38
racheedevel <git@rachee.dev>
2026-01-03 00:44:05 +0100

    relocated config to other repo

--- modules/home/rofi.nix
+++ modules/home/rofi.nix
@@ -1,11 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  xdg.configFile."rofi" = {
-    source = ../../configs/rofi;
-    recursive = true;
-    force = true;
-  };
+  # xdg.configFile."rofi" = {
+  #   source = ../../configs/rofi;
+  #   recursive = true;
+  #   force = true;
+  # };
 
   # Rofi install
   home.packages = with pkgs; [

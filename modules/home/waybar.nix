f6df9964419d4d77acb844db1f0ea85879bec118
racheedevel <git@rachee.dev>
2026-01-03 00:44:44 +0100

    relocated config to other repo

--- modules/home/waybar.nix
+++ modules/home/waybar.nix
@@ -2,11 +2,6 @@
 { config, pkgs, ... }:
 
 {
-  xdg.configFile."waybar" = {
-    source = ../../configs/waybar;
-    recursive = true;
-    force = true;
-  };
 
   # Hyprland-related user packages
   home.packages = with pkgs; [

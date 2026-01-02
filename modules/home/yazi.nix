c2ee82a8a7251b693db7538b599ffbe96b4753bc
racheedevel <git@rachee.dev>
2026-01-03 00:44:57 +0100

    relocated config to other repo

--- modules/home/yazi.nix
+++ modules/home/yazi.nix
@@ -6,28 +6,5 @@
     enableZshIntegration = true;
     shellWrapperName = "y";
 
-    settings = {
-      mgr = {
-        show_hidden = true;
-      };
-
-      preview = {
-        max_width = 1000;
-        max_height = 1000;
-      };
-    };
-
-    initLua = ''
-
-    '';
-
-    keymap = {
-      mgr.prepend_keymap = [
-        {
-          on = "<C-x>";
-          run = "shell -- dragon -x -i -T \"$0\"";
-        }
-      ];
-    };
   };
 }
